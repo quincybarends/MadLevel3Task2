2b52b94e1ca9292e1d1992ad93a9df6a8019cd6e
quincyba <qbarend@gmail.com>
2020-11-13 07:03:56 +0100

    added the screen to add portals

--- app/src/main/java/com/example/madlevel3task2/PortalFragment.kt
+++ app/src/main/java/com/example/madlevel3task2/PortalFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.navigation.fragment.findNavController
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -18,7 +17,7 @@ class PortalFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_portal, container, false)
+        return inflater.inflate(R.layout.fragment_portal_add, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
